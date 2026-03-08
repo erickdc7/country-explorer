dbc4af91b035959e1f60a87819100a388a9a034c
Erick Díaz <erickdiazc717@gmail.com>
2026-03-08 12:10:41 -0500

    docs(utils): add comment to cn utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Combina clases de Tailwind resolviendo conflictos entre ellas
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
